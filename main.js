c938858d2191d90155c30e22a62465863ce8f892
shalevwa23-meet <shalevax@gmail.com>
2022-07-25 10:12:30 +0300

    -

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeBackgroundColor()
+{
+	if(document.body.style.background == "yellow")
+	{
+		document.body.style.background = "blue";
+	}
+	else
+	{
+		document.body.style.background = "yellow";
+	}
+}
+
